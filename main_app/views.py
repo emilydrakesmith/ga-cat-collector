e59cc884aa946fd616453e40725faca0220a1c36
ManliestBen <benjamintmanley@gmail.com>
2021-05-03 13:08:41 -0500

    add cat model

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Cat
 
-class Cat:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', {'cats': cats})
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
